e3fbda41fe0a97650d33fb0d8dd30b87737d5024
Falynsky <nick2855@gmail.com>
2018-10-30 20:03:08 +0100

    Added REGEX's player name verification and handling exceptions.

--- PIOgame/src/Program.java
+++ PIOgame/src/Program.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Program {
-	
+
 	private static Scanner choose;
 
 	public static void main(String[] args) {
@@ -10,35 +10,41 @@ public class Program {
 		final Random rand = new Random();
 
 		int cube;
-		;;
 		choose = new Scanner(System.in);
-		//polimorfizm
+
 		Player player = null;
 		System.out.println("Who's the Player?\n\t1. Human\n\t2. Computer\n");
 		switch(choose.nextInt())
 		{
-		case 1: player = new PlayerHuman("Kamil");
-				break;
-		case 2: player = new PlayerComputer("Kamil");
-				break;
+		case 1: player = new PlayerHuman();
+		case 2: player = new PlayerComputer();
 		default:
-				break;
+			break;
 		}
-		
+		try{
+			//https://regex101.com/
+			player.setName("2Kamil");
 
-		//player1.setName("Kamil");
-		int guess;
-		do {
-			cube = rand.nextInt(6) + 1;
-			guess = player.guess();
+			int guess;
+			do {
+				cube = rand.nextInt(6) + 1;
+				guess = player.guess();
 
-			if (guess != cube)
-				System.out.println(
-						"Wrong " + player.getName() + "! Value of Cube = " + cube + " and Your guess = " + guess);
-			else
-				System.out.println("\nWell done " + player.getName() + "! You're right!");
+				if (guess != cube)
+					System.out.println(
+							"Wrong " + player.getName() + "! Value of Cube = " + cube + " and Your guess = " + guess);
+				else
+					System.out.println("\nWell done " + player.getName() + "! You're right!");
 
-		} while (guess != cube);
+			} while (guess != cube);
+		}
+//		catch (IllegalArgumentException e){
+//			System.err.println("\nWrong name human :/\n");
+//			//player.setName(choose.nextLine());
+//		}
+		catch (Exception e){
+			System.out.println("\nError." +player.toString());
+		}
 	}
 }
 
