756ba7d94d3469939c26eaad1e65816705803133
Student <Student@10.5.4.13>
2018-10-30 16:51:38 +0100

    Added REGEX and Exceptions

--- PIOgame/src/Player.java
+++ PIOgame/src/Player.java
@@ -2,6 +2,7 @@
 public abstract class Player {
 
 	private String name = "Player";
+	final String regex = "^[a-zA-Z][a-zA-Z0-9._@-]+$";
 
 	public Player() {
 	}
@@ -9,12 +10,20 @@ public abstract class Player {
 	public Player(String name) {
 		this.setName(name);
 	}
+	
+	@Override
+	public String toString()
+	{
+		return this.getClass().getSimpleName() + " : " +name; //PlayerComputer : Player
+		//return super.toString()+ " : " + name; //PlayerComputer@4554617c : Player
+	}
 
-	public void setName(String name) {
-		if (name != null && !name.isEmpty()) { // !"".equals(name) &&
-												// !name.isEmpty() for objects
+	public void setName(String name) { // !"".equals(name) && !name.isEmpty() for objects
+		if (name != null && name.matches(regex))  
 			this.name = name;
-		}
+		else { 
+			throw new IllegalArgumentException("Wrong name");
+			}
 	}
 
 	public String getName() {
